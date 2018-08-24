c2fda850e2eaed0f532705a03e11c0d37d47f1b0
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.local>
2018-08-24 13:21:47 -0400

    web cam implementation

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,18 +1,45 @@
 (function() {
-var video = document.getElementById("video");
-vendorURL = window.URL || window.webkitURL;
-navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-                        || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+        
+        var video;
+        var webcamStream;
+        
+        function startWebcam() {
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia ({
+                    video: true,
+                    audio: false
+                },
+                // successCallback
+                function(localMediaStream) {
+                    video = document.querySelector('video');
+                    video.src = window.URL.createObjectURL(localMediaStream);
+                    webcamStream = localMediaStream;
+                },
+                // errorCallback
+                function(err) {
+                    console.log("The following error occured: " + err);
+                });
+            } else {
+                console.log("getUserMedia not supported");
+            }  
+        }
+        
+        function stopWebcam() {
+            webcamStream.stop();
+        }
 
-navigator.getMedia({
-    video: true,
-    audio: false
-}, function (stream){
-    video.src = vendorURL.createObjectURL(stream);
-    video.play();
-}, function (error) {
-    console.log("error:", error);
-});
+        function qrcodeholder(){
+            qr.addData('Hi!');
+            qr.make();
+            document.getElementById("qrplaceHolder").innerHTML = qr.createImgTag();
+}
+
+qrcodeholder();
 
 })();
+
 
