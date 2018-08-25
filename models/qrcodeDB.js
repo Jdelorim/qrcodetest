19992a306730904e9a0940ea095263bed4b25490
Jdelorim <joshuadelorimier@gmail.com>
2018-08-25 10:03:37 -0400

    trying to pass a var in module export

--- models/qrcodeDB.js
+++ models/qrcodeDB.js
@@ -1,10 +1,49 @@
 var Sequelize = require("sequelize");
 var bodyParser = require("body-parser");
 var sequelize = require("../config/connection.js");
-var qrcodeData = require("../server.js");
+var QRcode = require('qrcode');
 
-console.log("getting from server:",qrcodeData.testVar);
+var fs = require("fs");
+var imageStore = "./images";
+var imgData;
+var base64Data;
+var siteToGenerate = "https://jd-drawing-app.herokuapp.com/"
+
+var testMe = "help";
+
+
+
+    QRcode.toDataURL(siteToGenerate, function(err, pic){
+        imgData = pic;
+      //  console.log("imgDATA:",imgData);
+        //regex to remove the heading and just get the base64 string
+         base64Data = imgData.replace(/^data:image\/png;base64,/,"");
+         console.log("baseDATA:",base64Data);
+       
+       //  writeME();
+      // tryme();
+    })
+       
+    
+
+    function tryme(){
+    console.log(base64Data);
+    }
+
+
+function writeME() {
+    fs.writeFile("./images/test####.png", base64Data, "base64", function(err){
+        if(err) {
+        console.log("file error:" ,err);
+        } else {
+        
+        console.log("wrote the file!");
+        }
+    });
+    }
+
 module.exports = function(app) {
+
 var QRcode = sequelize.define("qrcode_db", {
     name: Sequelize.STRING,
     image: Sequelize.TEXT
@@ -13,18 +52,22 @@ var QRcode = sequelize.define("qrcode_db", {
 //console.log(QRcode);
 QRcode.sync();
 //console.log("after sync:",QRcode);
+
+
 QRcode.create({
     name: "josh",
-    image: "kdsmfodfm"
+    image: base64Data
 });
 
 
+/*
 
     QRcode.findAll({}).then(function(results) {
-     //   console.log("QRcodeDBs:",results);
+       console.log("QRcodeDBs:",results);
       //  res.send(results);
     })
 
-
+*/
+//console.log(base64Data);
 
 }
--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var QRcode = require('qrcode');
+
 var app = express();
 var PORT = process.env.PORT || 4040;
 
 
-var fs = require("fs");
-var imageStore = "./images";
-var imgData;
-var base64Data;
-var siteToGenerate = "https://jd-drawing-app.herokuapp.com/";
-var testVar;
+
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-function codeReader() {
-    QRcode.toDataURL(siteToGenerate, function(err, pic){
-        imgData = pic;
-      //  console.log("imgDATA:",imgData);
-        //regex to remove the heading and just get the base64 string
-         base64Data = imgData.replace(/^data:image\/png;base64,/,"");
-        // console.log("baseDATA:",base64Data);
-       
-         writeME();
-    });
-}
-codeReader();
 
 
 //---------------------------
@@ -36,16 +20,7 @@ app.use(express.static("public"));
 
 
 
-function writeME() {
-fs.writeFile("./images/test####.png", base64Data, "base64", function(err){
-    if(err) {
-    console.log("file error:" ,err);
-    } else {
-    
-    console.log("wrote the file!");
-    }
-});
-}
+
 
 require("./models/qrcodeDB.js")(app);
 
@@ -54,6 +29,3 @@ function listening(){
     console.log("listening on port:", PORT);
 }
 
-module.export = {
-    testVar: "fsnfdnfhello"
-}
